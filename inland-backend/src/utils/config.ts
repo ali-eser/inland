80d1d399f5c42afbbe69e4e0ab38cf500291b3a8
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-01-25 00:07:15 +0300

    Define dotenv

--- inland-backend/src/utils/config.ts
+++ inland-backend/src/utils/config.ts
@@ -2,4 +2,10 @@ import { config } from "dotenv";
 
 config();
 
+export const DATABASE_NAME = process.env.DATABASE_NAME;
+export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+export const DATABASE_HOST = process.env.DATABASE_HOST;
+export const DATABASE_USER = process.env.DATABASE_USER;
+export const DATABASE_PORT = process.env.DATABASE_PORT;
+
 export const PORT = process.env.PORT;
