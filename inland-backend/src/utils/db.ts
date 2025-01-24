cf03cd717be50091a7eb53c981001a68a46fd9bc
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-01-25 02:53:16 +0300

    Add logging

--- inland-backend/src/utils/db.ts
+++ inland-backend/src/utils/db.ts
@@ -10,23 +10,25 @@ import {
   DATABASE_PORT
 } from "./config";
 
+console.log(DATABASE_HOST, DATABASE_PASSWORD, DATABASE_USER);
+
 console.log("connecting to the database");
 
 const sequelize = new Sequelize(
   `postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`, {
     dialect: "postgres",
-    logging: false,
+    logging: console.log,
     models: [User, Note]
   }
 );
 
 console.log("Database models added: ", sequelize.models);
 
-User.hasMany(Note);
 Note.belongsTo(User);
+User.hasMany(Note);
 
-User.sync({alter: true}).then(r => console.log(r));
 Note.sync({alter: true}).then(r => console.log(r));
+User.sync({alter: true}).then(r => console.log(r));
 
 export const connectToDB = async () => {
   try {
