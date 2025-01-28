fed190ab515a64982f199396f4fb1e0ca67cf8b7
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-01-28 22:13:20 +0300

    Implement userRouter

---
+++ inland-backend/src/controllers/userRouter.ts
@@ -0,0 +1,40 @@
+import userService from "../services/userService";
+import express from "express";
+
+const userRouter = express.Router();
+
+userRouter.get("/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+      res.status(400).json({ error: "Invalid user ID" });
+    }
+
+    const user = userService.fetchUser(id);
+
+    if (user) {
+      res.status(201).json(user);
+    } else {
+      res.status(404).json({ error: "User with specified ID not found"});
+    }
+  } catch (error) {
+    console.error("Error fetching user: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+userRouter.post("/", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (username.length >= 15) {
+      res.status(400).json({error: "Username cannot be longer than 15 characters"})
+    }
+    const userToAdd = await userService.addUser(username, email, password);
+    res.status(201).json(userToAdd);
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+});
+
+export default userRouter;
