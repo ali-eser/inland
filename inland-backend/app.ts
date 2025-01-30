d81a4ca854428a27f7dcef8a57b1ff7530fa0346
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-01-30 16:29:14 +0300

    Add route middleware

--- inland-backend/app.ts
+++ inland-backend/app.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import cors from "cors";
+import userRouter from "./src/controllers/userRouter";
+import noteRouter from "./src/controllers/noteRouter";
+import loginRouter from "./src/controllers/loginRouter";
 
 const app = express()
 
 app.use(express.json());
 app.use(cors());
+app.use("/api/user", userRouter);
+app.use("/api/note", noteRouter);
+app.use("/api/login", loginRouter);
 
 export default app;
