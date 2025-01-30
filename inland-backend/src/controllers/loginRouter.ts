305edcba58264c43e2e74f2a7857ddcb51213a73
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-01-30 13:54:57 +0300

    Slight refactor

--- inland-backend/src/controllers/loginRouter.ts
+++ inland-backend/src/controllers/loginRouter.ts
@@ -5,19 +5,18 @@ import express from "express";
 const loginRouter: express.Router = express.Router();
 
 loginRouter.post("/", async (req, res) => {
-  const { username, password } = req.body;
-  console.log(username, password)
-
   try {
+    const { username, password } = req.body;
+    console.log(username, password);
     const user = await loginService.login(username, password);
     res.status(200).json(user);
-  } catch (e) {
-    if (e instanceof AuthenticationError) {
+  } catch (error: any) {
+    if (error instanceof AuthenticationError) {
       res.status(400).json({ Error: "Invalid Credentials" });
     } else {
-      res.status(500).json({ Error: e });
+      res.status(500).json({ Error: error.message });
     }
   }
-})
+});
 
 export default loginRouter;
