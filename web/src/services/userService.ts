bd099cf47622c0f68e26f59c2c7e0fb40bb38bb9
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-02-04 16:11:35 +0300

    Implement user services on the frontend

---
+++ web/src/services/userService.ts
@@ -0,0 +1,26 @@
+const baseURL = import.meta.env.VITE_BASE_URL
+
+const login = async (data: object) => {
+  console.log("baseurl: ", baseURL);
+  const response = await fetch(`${baseURL}/api/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+
+  const jsonRes: object = await response.json();
+  return jsonRes;
+}
+
+const signUp = async (data: object) => {
+  const response = await fetch(`${baseURL}/api/user`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  });
+
+  const jsonRes: object = await response.json();
+  return jsonRes;
+}
+
+export default { login, signUp }
