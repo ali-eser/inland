3b96e593037e301faefc7d8b754c382ab2684c45
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-02-10 23:22:05 +0300

    Define redux store

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 import './index.css'
 import App from './App.tsx'
+import userSlice from './reducers/userReducer.ts'
+
+const store = configureStore({
+  reducer: {
+    user: userSlice
+  }
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
