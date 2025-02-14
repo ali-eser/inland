79be58ad4896099415a37751d38c7e7aabcf785b
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-02-14 03:25:41 +0300

    Add type User to reducer function

--- web/src/reducers/userReducer.ts
+++ web/src/reducers/userReducer.ts
@@ -7,7 +7,7 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser(_state, action: PayloadAction<User>) {
+    setUser(_state, action: PayloadAction<User>): User {
       return action.payload;
     }
   }
