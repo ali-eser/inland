24cc98011b4d7b37488cdcb51c68017a191eb16b
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-02-18 22:29:57 +0300

    Add NoteApp route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router"
 import Login from "@/components/Login"
+import NoteApp from "@/components/NoteApp"
 
 const App = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<NoteApp />} />
         <Route path="/auth" element={<Login />} />
       </Routes>
     </>
