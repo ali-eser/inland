49b736d26cbe0fc7fc773a3df7d8d5187c2e3e30
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-02-19 16:37:17 +0300

    Return after responses

--- inland-backend/src/controllers/noteRouter.ts
+++ inland-backend/src/controllers/noteRouter.ts
@@ -9,14 +9,18 @@ noteRouter.get("/:id", async (req, res) => {
     const id = parseInt(req.params.id);
     if (isNaN(id)) {
       res.status(400).json({ error: "Invalid user ID. ID must be an integer" });
+      return;
     }
     const notes = await noteService.fetchNotesByUser(id);
-    res.status(204).json({ notes: notes });
+    res.status(201).json({ notes: notes });
+    return;
   } catch (error: any) {
     if (error instanceof NotFoundError) {
       res.status(error.statusCode).json({ error: error.message });
+      return;
     } else {
       res.status(500).json({ error: error.message });
+      return;
     }
   }
 });
@@ -26,15 +30,19 @@ noteRouter.get("/single/:id", async (req, res) => {
     const id = parseInt(req.params.id);
     if (isNaN(id)) {
       res.status(400).json({ error: "Invalid note ID. ID must be an integer." });
+      return;
     }
 
     const note = await noteService.fetchSingleNote(id);
     res.status(201).json(note);
+    return;
   } catch (error: any) {
     if (error instanceof NotFoundError) {
       res.status(error.statusCode).json({ error: error.message });
+      return;
     } else {
       res.status(500).json({ error: error.message });
+      return;
     }
   }
 });
@@ -43,23 +51,28 @@ noteRouter.post("/", async (req, res) => {
   try {
     if (!req.body.userId) {
       res.status(400).json({ error: "A user ID must be specified" });
+      return;
     }
     const userId: number = parseInt(req.body.userId);
     if (isNaN(userId)) {
       res.status(400).json({ error: "Invalid user ID. ID must be an integer." });
+      return;
     }
 
     if (req.body.title) {
       const { title, content } = req.body;
       const newNote = await noteService.createNote(userId, content, title);
-      res.status(204).json({ newNote });
+      res.status(201).json(newNote);
+      return;
     } else {
       const content: string = req.body.content;
       const newNote = await noteService.createNote(userId, content);
-      res.status(204).json({ newNote });
+      res.status(201).json(newNote);
+      return;
     }
   } catch (error: any) {
     res.status(500).json({ error: error.message });
+    return;
   }
 });
 
@@ -68,16 +81,20 @@ noteRouter.put("/:id", async (req, res) => {
     const id: number = parseInt(req.params.id);
     if (isNaN(id)) {
       res.status(400).json({ error: "Invalid note ID. ID must be an integer." });
+      return;
     }
 
     const content: string = req.body.content;
     const updatedNote = await noteService.updateNote(id, content);
     res.status(204).json({ updatedNote });
+    return;
   } catch (error: any) {
     if (error instanceof NotFoundError) {
       res.status(error.statusCode).json({error: error.message});
+      return;
     } else {
       res.status(500).json({ error: error.message });
+      return;
     }
   }
 });
@@ -87,15 +104,19 @@ noteRouter.delete("/:id", async (req, res) => {
     const id: number = parseInt(req.params.id);
     if (isNaN(id)) {
       res.status(400).json({ error: "Invalid note ID. ID must be an integer." });
+      return;
     }
 
     await noteService.deleteNote(id);
     res.status(204).json({ message: `The note with ID ${id} deleted successfully` });
+    return;
   } catch (error: any) {
     if (error instanceof NotFoundError) {
       res.status(error.statusCode).json({error: error.message});
+      return;
     } else {
       res.status(500).json({ error: error.message });
+      return;
     }
   }
 });
