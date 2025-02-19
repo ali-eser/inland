32f2a14bfb57794e47acca2907c540da5bbfacb5
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-02-19 20:09:36 +0300

    Define fetchNotes for fetching on the React app

---
+++ web/src/services/noteService.ts
@@ -0,0 +1,19 @@
+const baseURL = import.meta.env.VITE_BASE_URL;
+
+const fetchNotes = async (id: number) => {
+  try {
+    const response = await fetch(`${baseURL}/api/note/${id}`, {
+      method: "GET"
+    });
+
+    if (response.ok) {
+      const data= await response.json();
+      return data;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+}
+
+export default { fetchNotes };
