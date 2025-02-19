5ba5d1bcd6f1b111f9ddba9f3e68fed1225d51c9
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-02-19 16:37:20 +0300

    Return after responses

--- inland-backend/src/controllers/userRouter.ts
+++ inland-backend/src/controllers/userRouter.ts
@@ -9,16 +9,19 @@ userRouter.get("/:id", async (req, res) => {
     const id = parseInt(req.params.id);
     if (isNaN(id)) {
       res.status(400).json({ error: "Invalid user ID. ID must be an integer." });
+      return;
     }
 
-    const user = userService.fetchUser(id);
+    const user = await userService.fetchUser(id);
     res.status(201).json(user);
+    return;
   } catch (error: any) {
     if (error instanceof NotFoundError) {
-      res.status(error.statusCode).json({ error: error.message });
+      res.status(404).json({ error: error.message });
+      return;
     }
-    console.error("Error fetching user: ", error);
     res.status(500).json({ error: "Internal server error" });
+    return;
   }
 });
 
@@ -27,15 +30,19 @@ userRouter.post("/", async (req, res) => {
     const { username, email, password, passwordConf } = req.body;
 
     if (username.length >= 15) {
-      res.status(400).json({error: "Username cannot be longer than 15 characters"})
+      res.status(400).json({error: "Username cannot be longer than 15 characters"});
+      return;
     }
     const userToAdd = await userService.addUser(username, email, password, passwordConf);
     res.status(201).json(userToAdd);
+    return;
   } catch (error: any) {
     if (error.message.includes("provided username or email")) {
       res.status(400).json({ error: error.message });
+      return;
     } else {
       res.status(500).json({ error: error.message });
+      return;
     }
   }
 });
@@ -45,11 +52,14 @@ userRouter.delete("/:id", async (req, res) => {
     const id = parseInt(req.params.id);
     if (isNaN(id)) {
       res.status(400).json({ error: "Invalid user ID. ID must be an integer." });
+      return;
     }
     await userService.removeUser(id);
     res.status(204).json({message: `User with ID ${id} removed successfully`})
+    return;
   } catch (error: any) {
     res.status(500).json({ error: error.message });
+    return;
   }
 });
 
