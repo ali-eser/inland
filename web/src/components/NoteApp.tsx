6ed5250f7d0953f1a425093154ac46ec48bf5f86
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-02-20 18:25:39 +0300

    Implement fetching and setting of notes

--- web/src/components/NoteApp.tsx
+++ web/src/components/NoteApp.tsx
@@ -1,16 +1,45 @@
 import { useNavigate } from "react-router";
 import AppSidebar from "./layout/AppSidebar";
+import noteService from "@/services/noteService";
+import { useSelector, useDispatch  } from "react-redux";
+import { Note } from "@/types";
+import { useEffect } from "react";
+import { setNotes } from "@/reducers/noteReducer";
+import { setUser } from "@/reducers/userReducer";
 
 const NoteApp = () => {
   const navigate = useNavigate();
-  const loggedUser= window.localStorage.getItem('loggedUser');
-  if (!loggedUser) {
-    navigate('/auth');
+  const dispatch = useDispatch();
+  const noteState: Note[] = useSelector(({ notes }: { notes: Note[] }): Note[] => notes);
+  const loggedUser = window.localStorage.getItem("loggedUser");
+  const loggedUserID = window.localStorage.getItem("loggedUserID");
+
+  const handleLogout = () => {
+    window.localStorage.clear();
+    dispatch(setUser(null));
+    navigate("/");
   }
-  console.log(loggedUser)
+
+  useEffect(() => {
+    if (!loggedUser || !loggedUserID) {
+      navigate('/auth');
+      return;
+    }
+
+    const getNotes = async () => {
+      try {
+        const response = await noteService.fetchNotes(parseInt(loggedUserID as string));
+        dispatch(setNotes(response.notes));
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getNotes();
+  }, [dispatch, loggedUser, loggedUserID, navigate])
+
   return (
     <>
-      <AppSidebar loggedUser={loggedUser}/>
+      <AppSidebar loggedUser={loggedUser} notes={noteState} handleLogout={handleLogout} />
     </>
   )
 }
