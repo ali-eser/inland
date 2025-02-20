60ab20556cae1f148e04e181888a17c02b1fa2ad
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-02-20 21:39:51 +0300

    Format dates before displaying

--- web/src/components/layout/AppSidebar.tsx
+++ web/src/components/layout/AppSidebar.tsx
@@ -25,23 +25,41 @@ import {
 import { Note } from "@/types";
 
 const AppSidebar = ({ loggedUser, notes, handleLogout }: { loggedUser: string | null, notes: Note[], handleLogout: () => void }) => {
+  const formatDate = (dateString: string) => {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const date = new Date(dateString);
+    return days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + " at "  + date.getHours() + ":" + date.getMinutes()
+  }
+
+  const formattedNotes = notes.map(n => {
+    return {...n, updatedAt: formatDate(n.updatedAt), createdAt: formatDate(n.createdAt)}
+  });
+
   return (
     <SidebarProvider>
       <Sidebar>
-        <SidebarHeader style={{fontFamily: "instrument serif", fontSize: "2em"}}>inland.</SidebarHeader>
+        <SidebarHeader style={{fontFamily: "instrument serif", fontSize: "2em", marginLeft: "0.5rem"}}>inland.</SidebarHeader>
         <SidebarContent>
           <SidebarGroup>
             <SidebarGroupLabel>Recent</SidebarGroupLabel>
             <SidebarGroupContent>
               <SidebarMenu>
-                {notes.length > 0 ? ( notes.map(n => (
+                {formattedNotes.length > 0 ? ( formattedNotes.map(n => (
                   <SidebarMenuItem key={n.id}>
-                    <SidebarMenuButton asChild>
+                    <SidebarMenuButton asChild style={{ paddingTop: 25, paddingBottom: 25 }}>
                       <a href={""}>
-                        <span>{n.content}</span>
+                        <div style={{ display: "flex", flexDirection: "column" }}>
+                          {n.content.length > 30 ? (
+                            <span>{n.content.slice(0, 30) + "..."}</span>
+                          ): (
+                            <span>{n.content}</span>
+                          )}
+                          <span style={{ fontSize: "0.67em" }}>{n.updatedAt}</span>
+                        </div>
                       </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
                 ))) : (
                   <SidebarMenuItem>
                     <SidebarMenuSubButton>You don't have anything to show!</SidebarMenuSubButton>
