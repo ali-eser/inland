2d65f742eb84e45e70e46f82c17a11fb1b9d8ebb
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-03-18 18:21:13 +0300

    Handle auto syncing of notes upon typing

--- web/src/components/layout/Editor.tsx
+++ web/src/components/layout/Editor.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { useEditor, EditorContent } from "@tiptap/react"
+import { useDispatch } from "react-redux";
+import { setNotes } from "@/reducers/noteReducer";
 import StarterKit from "@tiptap/starter-kit"
 import Typography from "@tiptap/extension-typography"
 import { Note } from "@/types";
 
 const extensions = [StarterKit, Typography]
 
-const Editor = ({ note }: { note: Note | Record<string, never> }) => {
+const Editor = ({ note, noteState, handleNotePut }: { note: Note, noteState: Note[], handleNotePut: (n: Note) => void }) => {
+  const dispatch = useDispatch();
 
   const editor = useEditor({
     extensions,
@@ -24,6 +27,32 @@ const Editor = ({ note }: { note: Note | Record<string, never> }) => {
     }
   }, [editor, note]);
 
+  useEffect(() => {
+    if (editor) {
+
+      editor.on('update', ({ editor }) => {
+        const now = new Date;
+        const updatedNote: Note = {
+          ...note,
+          updatedAt: now.toISOString(),
+          content: editor.getHTML()
+        };
+
+        const updatedNoteState: Note[] = noteState.map((existingNote) =>
+          existingNote.id === note.id ? updatedNote : existingNote
+        );
+        console.log(noteState)
+        dispatch(setNotes(updatedNoteState));
+        handleNotePut(updatedNote);
+      });
+
+
+      return () => {
+        editor.off('update');
+      };
+    }
+  }, [editor, note, handleNotePut])
+
   return (
       <div style={{
         display: 'flex',
@@ -33,7 +62,7 @@ const Editor = ({ note }: { note: Note | Record<string, never> }) => {
         flexDirection: 'column',
         justifyContent: 'flex-start',
         alignItems: 'center',
-        overflow: 'hidden'
+        overflow: 'hidden',
       }}>
         <p
           style={{ fontSize: "0.77em" }}
@@ -41,7 +70,7 @@ const Editor = ({ note }: { note: Note | Record<string, never> }) => {
         >
           {note.updatedAt}
         </p>
-        <EditorContent spellCheck="false" className="editor-content" editor={editor} />
+        <EditorContent spellCheck="false" className="editor-content" style={{ wordBreak: "break-word" }} editor={editor} />
       </div>
   )
 };
