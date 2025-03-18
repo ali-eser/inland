9ba9113e12c22a6f513392389d66c6db19b2326e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-03-18 18:21:49 +0300

    Rename to utils.ts

--- web/src/utils/formatDate.ts
+++
@@ -1,6 +0,0 @@
-export const formatDate = (dateString: string): string => {
-  const days: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  const date: Date = new Date(dateString);
-  return days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + " at "  + date.getHours() + ":" + date.getMinutes();
-}
