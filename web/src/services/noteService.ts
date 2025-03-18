542edbe231857ab1880f94b8988593566b6b5d0e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-03-18 04:39:20 +0300

    Add note service for updating notes

--- web/src/services/noteService.ts
+++ web/src/services/noteService.ts
@@ -1,3 +1,5 @@
+import { Note } from "@/types";
+
 const baseURL = import.meta.env.VITE_BASE_URL;
 
 const fetchNotes = async (id: number) => {
@@ -13,7 +15,27 @@ const fetchNotes = async (id: number) => {
   } catch (err) {
     console.error(err);
   }
-
 }
 
-export default { fetchNotes };
+const updateNote = async (note: Note) => {
+  try {
+    const response = await fetch(`${baseURL}/api/note/${note.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(note)
+    });
+
+    console.log(response)
+
+
+      const data = await response.json();
+      console.log(data);
+      return data;
+
+  } catch(error) {
+    console.error(error);
+    return;
+  }
+}
+
+export default { fetchNotes, updateNote };
