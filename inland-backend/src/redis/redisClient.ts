33c7daba5b3f4f675ffd09cbb6a37ea2b6e28b0c
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-03-20 17:18:44 +0300

    Define Redis connection

---
+++ inland-backend/src/redis/redisClient.ts
@@ -0,0 +1,18 @@
+import Redis from "ioredis";
+import { REDIS_URL, REDIS_PORT } from "../utils/config";
+
+const port: number = REDIS_PORT ? parseInt(REDIS_PORT) : 6379;
+const url: string = REDIS_URL ? REDIS_URL : "127.0.0.1";
+
+const redisClient = new Redis(port, url);
+
+export const connectToRedis =  async () => {
+  try {
+    console.log("Connecting to Redis");
+    await redisClient.ping();
+    console.log(`Connected to Redis at ${url}:${port}`);
+  } catch (err) {
+    console.error("Redis connection error: ", err);
+    process.exit(1);
+  }
+};
