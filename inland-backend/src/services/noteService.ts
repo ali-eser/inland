a7199d4ec882580c6862be6f78bebe35f5c5f099
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-03-24 21:11:41 +0300

    Complete Redis implementation

--- inland-backend/src/services/noteService.ts
+++ inland-backend/src/services/noteService.ts
@@ -2,24 +2,47 @@ import Note from "../models/note";
 import { NotFoundError } from "../../exceptions/NotFoundError";
 import { redisClient } from "../redis/redisClient";
 
+const fetchAllFromCache = async (userId: number) => {
+  try {
+    const noteIdsKey = `user:${userId}:noteIds`;
+    const noteIds = await redisClient.smembers(noteIdsKey);
+
+    if (noteIdsKey.length === 0) {
+      return;
+    }
+
+    const noteKeys: string[] = noteIds.map(noteId => `user:${userId}:note:${noteId}`);
+    const cachedNotes = await redisClient.mget(...noteKeys);
+
+    const notes = cachedNotes.filter(note => note !== null).map(note => JSON.parse(note));
+    return notes;
+  } catch (error) {
+    console.error("Error fetching notes from cache: ", error);
+  }
+};
+
 const fetchNotesByUser = async (userId: number) => {
   try {
-    const redisKey = `user:${userId}:notes`;
-    const cachedNotes = await redisClient.get(redisKey);
+    const cachedNotes = await fetchAllFromCache(userId);
 
-    if (cachedNotes !== null) {
-      console.log("Cache hit!: ", cachedNotes);
-      return JSON.parse(cachedNotes);
+    // console.log(typeof(cachedNotes));
+    if (cachedNotes && cachedNotes.length > 0) {
+      console.log(`Serving ${cachedNotes.length} notes from Redis for user ${userId}`);
+      return cachedNotes;
+    } else {
+      console.log(`Cache miss or empty cache for user ${userId}, fetching from database`);
+      const notes: Note[] | null = await Note.findAll({ where: { userId: userId } });
+
+      const noteIdsKey = `user:${userId}:noteIds`;
+      if (notes && notes.length > 0) {
+        for (let i = 0; i < notes.length; i++) {
+          await redisClient.setex(`user:${userId}:note:${notes[i].id}`, 3600, JSON.stringify(notes[i]));
+          await redisClient.sadd(noteIdsKey, notes[i].id);
+        }
+        console.log(`Populated Redis cache with ${notes.length} notes for user ${userId}`);
+      }
+      return notes;
     }
-
-    console.log("Cache miss, fetching from database");
-    const notes: Note[] | null = await Note.findAll({ where: { userId: userId } });
-
-    if (notes && notes.length > 0) {
-      await redisClient.setex(redisKey, 3600, JSON.stringify(notes));
-      console.log("Notes cached in Redis");
-    }
-    return notes;
   } catch (error) {
     console.error("Error: ", error);
     throw error;
@@ -50,13 +73,25 @@ const createNote = async (userId: number, content: string, title?: string) => {
         content: content,
         title: title
       }
-      return await Note.create(newNote);
+      const addedNote =  await Note.create(newNote);
+      const noteKey = `user:${userId}:note:${addedNote.id}`;
+      const noteIdsKey = `user:${userId}:noteIds`;
+
+      await redisClient.setex(noteKey, 3600, JSON.stringify(addedNote));
+      await redisClient.sadd(noteIdsKey, addedNote.id);
+      return addedNote;
     } else {
       const newNote =  {
         userId: userId,
         content: content
       }
-      return await Note.create(newNote);
+      const addedNote =  await Note.create(newNote);
+      const noteKey = `user:${userId}:note:${addedNote.id}`;
+      const noteIdsKey = `user:${userId}:noteIds`;
+
+      await redisClient.setex(noteKey, 3600, JSON.stringify(addedNote));
+      await redisClient.sadd(noteIdsKey, addedNote.id);
+      return addedNote;
     }
   } catch (error) {
     console.log("Error: ", error);
@@ -67,10 +102,16 @@ const createNote = async (userId: number, content: string, title?: string) => {
 const updateNote = async (noteId: number, content: string) => {
   try {
     const note: Note | null = await Note.findByPk(noteId);
+
     if (!note) {
       throw new Error(`The note with the specified ID ${noteId} does not exist`);
     }
-    const updatedNote = await note.update({ content: content })
+
+    const updatedNote = await note.update({ content: content });
+    const cacheKey = `user:${updatedNote.userId}:note:${updatedNote.id}`;
+
+    await redisClient.setex(cacheKey, 3600, JSON.stringify(updatedNote));
+
     return updatedNote;
   } catch (error) {
     console.error("Error: ", error);
@@ -81,9 +122,16 @@ const updateNote = async (noteId: number, content: string) => {
 const deleteNote = async (noteId: number) => {
   try {
     const note: Note | null = await Note.findByPk(noteId);
+
     if (!note) {
       throw new NotFoundError(`The note with the specified ID ${noteId} does not exist`);
     }
+
+    const noteKey = `user:${note.userId}:note:${noteId}`;
+    const noteIdsKey = `user:${note.userId}:noteIds`;
+
+    await redisClient.del(noteKey);
+    await redisClient.srem(noteIdsKey, noteId);
 
     return await note.destroy();
   } catch (error) {
