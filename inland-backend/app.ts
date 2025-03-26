5d72af9c625a06fd53766c66269f4ce612e6fb17
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-03-27 00:35:18 +0300

    Fix wrong origin

--- inland-backend/app.ts
+++ inland-backend/app.ts
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
