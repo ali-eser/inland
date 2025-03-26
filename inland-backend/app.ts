19e6b181c61acedca6e202c94641d88a6a701ba9
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-03-26 23:58:22 +0300

    Update cors settings

--- inland-backend/app.ts
+++ inland-backend/app.ts
@@ -10,7 +10,9 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-  credentials: true
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
 app.use("/api/auth", authRouter);
