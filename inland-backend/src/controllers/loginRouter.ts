e53ab3e5e1a5206fd11d90199c8759bb7a679ab5
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-03-27 00:35:38 +0300

    Make cookie settings conditional

--- inland-backend/src/controllers/loginRouter.ts
+++ inland-backend/src/controllers/loginRouter.ts
@@ -12,14 +12,14 @@ loginRouter.post("/", async (req, res) => {
     res.cookie("accessToken", user.accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 60 * 60 * 1000,
     });
 
     res.cookie("refreshToken", user.refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
