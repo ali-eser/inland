b372c17086e1523ce56cd5fb737ec9dfac930055
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-03-29 04:12:07 +0300

    Implement base64 to array and vice versa

--- web/src/utils/utils.ts
+++ web/src/utils/utils.ts
@@ -1,9 +1,22 @@
 export const formatDate = (dateString: string): string => {
   const days: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  const months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const months: string[] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
   const date: Date = new Date(dateString);
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes: string = String(date.getMinutes()).padStart(2, "0");
+  const hours: string = String(date.getHours()).padStart(2, "0");
   return days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + " at " + hours + ":" + minutes;
 }
 
@@ -11,4 +24,26 @@ export const getTextFromHtml = (html: string): string => {
   const temp = document.createElement('div');
   temp.innerHTML = html;
   return temp.textContent || temp.innerText || '';
-};
+}
+
+export const arrayBufferToBase64 = (buffer: ArrayBuffer) => {
+  let binary: string = '';
+  const bytes = new Uint8Array(buffer);
+
+  for (let i = 0; i < buffer.byteLength; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+
+  return window.btoa(binary);
+}
+
+export const base64toArrayBuffer = (base64: string) => {
+  const binary = window.atob(base64);
+  const bytes = new Uint8Array(binary.length);
+
+  for (let i = 0; i < binary.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+
+  return bytes.buffer;
+}
