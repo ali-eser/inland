212651843e65e4624b6d5b3c6eb62c21ab7ba602
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-03-31 23:06:39 +0300

    Implement global notification system

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,27 @@
 import { Routes, Route } from "react-router"
 import Login from "@/components/Login"
 import NoteApp from "@/components/NoteApp"
+import { NotificationState } from "./types"
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { useSelector } from "react-redux"
+import { useEffect } from "react"
 
 const App = () => {
+  const notification = useSelector(({ notification }: { notification: NotificationState }): NotificationState => notification);
+  useEffect(() => {
+    console.log(notification);
+  }, [notification]);
   return (
     <>
+      {notification && (
+        <Alert variant={notification.type} className="alert">
+          <AlertTitle>{notification.title}</AlertTitle>
+          <AlertDescription>
+            {notification.description}
+          </AlertDescription>
+        </Alert>
+      )}
       <Routes>
         <Route path="/" element={<NoteApp />} />
         <Route path="/auth" element={<Login />} />
