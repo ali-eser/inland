1136a7d60bf94de096c5a9ebc9a6071ef692dba8
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-04-01 21:01:03 +0300

    Import interface Notification

--- web/src/reducers/notificationReducer.ts
+++ web/src/reducers/notificationReducer.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Notification } from "@/types";
 
 type NotificationState = Notification | null;
 const initialState = null as NotificationState;
