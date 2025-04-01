a895eac0ac4e6b7722ba0cab69819089ae6fb6b0
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-04-01 04:27:05 +0300

    Move encryption to userService.ts

--- web/src/services/userService.ts
+++ web/src/services/userService.ts
@@ -1,4 +1,14 @@
 const baseURL = import.meta.env.VITE_BASE_URL;
+import { NewUser } from "@/types";
+import { arrayBufferToBase64 } from "@/utils/utils";
+
+import {
+  generateSalt,
+  deriveKeyFromPass,
+  generateMasterKey,
+  encryptMasterKey,
+  verifyEncryptionPassword
+} from "@/utils/cryptography"
 
 const login = async (data: object) => {
   const response = await fetch(`${baseURL}/api/login`, {
@@ -11,11 +21,28 @@ const login = async (data: object) => {
   return jsonRes;
 }
 
-const signUp = async (data: object) => {
+const signUp = async (data: NewUser) => { 
+  const salt = await generateSalt();
+  const derivedKey: CryptoKey = await deriveKeyFromPass(data.password, arrayBufferToBase64(salt));
+  const masterKey: CryptoKey = await generateMasterKey();
+  const encryptedMasterKey: string = await encryptMasterKey(masterKey, derivedKey);
+  console.log(await verifyEncryptionPassword(
+    data.password,
+    encryptedMasterKey,
+    arrayBufferToBase64(salt)
+  ));
+
+  const userToAdd = {
+    ...data,
+    keyDerivationSalt: arrayBufferToBase64(salt),
+    encryptedMasterKey: encryptedMasterKey
+  };
+  console.log(userToAdd);
+
   const response = await fetch(`${baseURL}/api/user`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
+    body: JSON.stringify(userToAdd)
   });
 
   const jsonRes = await response.json();
