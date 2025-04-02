2d77277bbfc4e0018f482ee67a274fc31b19444e
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-04-03 01:03:46 +0300

    Improve error handling

--- web/src/services/noteService.ts
+++ web/src/services/noteService.ts
@@ -37,13 +37,16 @@ const createNote = async (note: NewNote) => {
 
     if (!response.ok) {
       const errorJson = await response.json();
-      return { error: errorJson.error || "An unspecified error occurred while creating a new note" };
+      return { error: errorJson.error || "An unspecified error occurred while creating a new note." };
     }
 
     return await response.json();
   } catch (error) {
     console.error(error);
-    return { error: error.message || 'An error occurred while creating a note' }
+    if (error instanceof Error) {
+      return { error: error.message };
+    }
+    return { error: 'An error occurred while creating a note.' }
   }
 }
 
@@ -58,11 +61,35 @@ const updateNote = async (note: Note) => {
     const data = await response.json();
     console.log(data);
     return data;
-
   } catch(error) {
     console.error(error);
-    return;
+    if (error instanceof Error) {
+      return { error: error.message };
+    }
+    return { error: 'An error occurred while saving.' }
   }
 }
 
-export default { fetchNotes, updateNote, createNote };
+const deleteNote = async (id: number) => {
+  try {
+    const response = await fetch(`${baseURL}/api/note/${id}`, {
+      method: "DELETE",
+      credentials: "include"
+    });
+
+    if (!response.ok) {
+      const errorJson = await response.json();
+      return { error: errorJson.error || "An unspecified error occurred while deleting." };
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      return { error: error.message };
+    }
+    return { error: 'An error occurred while deleting.' }
+  }
+}
+
+export default { fetchNotes, updateNote, createNote, deleteNote };
