d6916c972b635ee165701db44be089966810bbe2
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-04-02 20:52:51 +0300

    Add placeholder for empty notes and enable new note button

--- web/src/components/layout/AppSidebar.tsx
+++ web/src/components/layout/AppSidebar.tsx
@@ -25,30 +25,42 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { LucideFilePlus2, LucideSave } from "lucide-react"; 
 import { ModeToggle } from "../mode-toggle";
-import { getTextFromHtml, formatDate } from "@/utils/utils";
+import { getTextFromHtml } from "@/utils/utils";
 import { Note } from "@/types";
 
-const AppSidebar = ({ loggedUser, notes, handleLogout, handleSelectedNote }: { loggedUser: string | null, notes: Note[], handleLogout: () => void, handleSelectedNote: (n: Note) => void }) => {
-  const formattedNotes = notes.map(n => {
-    return {...n, updatedAt: formatDate(n.updatedAt), createdAt: formatDate(n.createdAt)}
-  });
-
+const AppSidebar = ({
+  loggedUser,
+  notes,
+  handleLogout,
+  handleSelectedNote,
+  handleCreateNote
+}: {
+  loggedUser: string | null,
+  notes: Note[],
+  handleLogout: () => void,
+  handleSelectedNote: (n: Note) => void,
+  handleCreateNote: () => void
+}) => {
   return (
     <SidebarProvider>
       <Sidebar>
         <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginLeft: "0.5rem", marginRight: "0.5rem",  marginBottom: "-16px" }}>
           <SidebarHeader style={{ fontFamily: "sacramento", fontSize: "2.3em" }}>inland.</SidebarHeader>
           <div style={{ display: "flex", marginBottom: 7 }}>
-            <Button variant="ghost" size="sm"><LucideFilePlus2 className="h-[1.2rem] w-[1.2rem]"/></Button>
+            <Button
+              variant="ghost"
+              size="sm"
+              onClick={() => handleCreateNote()}
+            ><LucideFilePlus2 className="h-[1.2rem] w-[1.2rem]"/></Button>
             <Button variant="ghost" size="sm"><LucideSave className="h-[1.2rem] w-[1.2rem]" /></Button>
           </div>
         </div>
         <SidebarContent>
           <SidebarGroup>
-            <SidebarGroupLabel>RECENT</SidebarGroupLabel>
+            <SidebarGroupLabel>RECENTS</SidebarGroupLabel>
             <SidebarGroupContent>
               <SidebarMenu>
-                {formattedNotes.length > 0 ? ( formattedNotes.map(n => (
+                {notes.length > 0 ? (notes.map(n => (
                   <SidebarMenuItem key={n.id}>
                     <SidebarMenuButton asChild
                       style={{ paddingTop: 25, paddingBottom: 25 }}
@@ -60,7 +72,9 @@ const AppSidebar = ({ loggedUser, notes, handleLogout, handleSelectedNote }: { l
                         <div style={{ display: "flex", flexDirection: "column" }}>
                           {getTextFromHtml(n.content).length > 27 ? (
                             <span>{getTextFromHtml(n.content).slice(0, 27) + "..."}</span>
-                          ): (
+                          ): getTextFromHtml(n.content).length === 0 ? (
+                            <span>New Note</span>
+                          ) : (
                             <span>{getTextFromHtml(n.content)}</span>
                           )}
                           <span style={{ fontSize: "0.67em" }}>{n.updatedAt}</span>
@@ -70,7 +84,7 @@ const AppSidebar = ({ loggedUser, notes, handleLogout, handleSelectedNote }: { l
                   </SidebarMenuItem>
                 ))) : (
                   <SidebarMenuItem>
-                    <SidebarMenuSubButton>You don't have anything to show!</SidebarMenuSubButton>
+                    <SidebarMenuSubButton>You do not have any saved notes</SidebarMenuSubButton>
                   </SidebarMenuItem>
                 )}
               </SidebarMenu>
